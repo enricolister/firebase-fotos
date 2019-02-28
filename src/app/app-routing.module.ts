1d76c92fcd2810ed0c093581a7ab017b41209c44
Enrico Lister <enricolister@hotmail.com>
2019-03-01 00:57:26 +0100

    Created components and routes struct

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { CargaComponent } from './components/carga/carga.component';
+import { FotosComponent } from './components/fotos/fotos.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const ROUTES: Routes = [
+  { path: 'fotos', component: FotosComponent },
+  { path: 'carga', component: CargaComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'fotos' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(ROUTES)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
