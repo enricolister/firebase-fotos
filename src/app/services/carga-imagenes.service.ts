26780ad42ff932d224246b39756fc50ef85028a1
Enrico Lister <enricolister@hotmail.com>
2019-03-09 01:42:33 +0100

    End of load process

--- src/app/services/carga-imagenes.service.ts
+++ src/app/services/carga-imagenes.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFirestore } from '@angular/fire/firestore';
 import * as firebase from 'firebase';
+import { puts } from 'util';
+import { isNgTemplate } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
@@ -13,17 +15,42 @@ export class CargaImagenesService {
 
   constructor(private db: AngularFirestore) {}
 
-    private guardarImagen(
-      imagen: {
-        nombre: string,
-        url: string,
-      }
-    ) {
-      this.db.collection(`/${this.CARPETA_IMAGENES}`)
-      .add(imagen);
+  private guardarImagen(
+    imagen: {
+      nombre: string,
+      url: string,
     }
+  ) {
+    this.db.collection(`/${this.CARPETA_IMAGENES}`)
+    .add(imagen);
+  }
 
-    cargarImagenesFirebase(imagenes: FileItem[]) {
-        console.log(imagenes);
+  cargarImagenesFirebase(imagenes: FileItem[]) {
+    const storageRef = firebase.storage().ref();
+    for (const imagen of imagenes) {
+      imagen.estaSubiendo = true;
+      if (imagen.progreso >= 100) {
+        continue;
+      }
+      const uploadTask: firebase.storage.UploadTask =
+        storageRef.child(`${this.CARPETA_IMAGENES}/${imagen.nombreArchivo}`)
+          .put(imagen.archivo);
+
+      uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+      (snapshot: firebase.storage.UploadTaskSnapshot) => imagen.progreso =
+        (snapshot.bytesTransferred / snapshot.totalBytes) * 100,
+      (error) => console.log('Error al subir: ', error),
+      () => {
+        console.log('Imagen cargada correctamente');
+        uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+          imagen.url = downloadURL;
+          imagen.estaSubiendo = false;
+          this.guardarImagen({
+            nombre: imagen.nombreArchivo,
+            url: imagen.url
+          });
+        });
+      });
     }
+  }
 }
